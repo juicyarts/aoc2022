7646904647ad5af5ff23e4f0c84e1a98f6a90998
huess <yildiz.h@eppendorf.de>
2024-01-15 21:47:14 +0100

    feat(day1): trying another approach

--- d1/bin/main.ml
+++ d1/bin/main.ml
@@ -4,25 +4,20 @@ open Str
 let filename = Sys.argv.(1)
 
 let p1 file =
-    let contents = In_channel.with_open_bin file In_channel.input_all in
-    fold_left (fun (a, i) b -> if i < 1 then (a+b, i+1) else (a, i+1)) (0,0)
-    (rev (sort Int.compare (map (fun s ->
-        fold_left (+) 0 (
-            map (int_of_string) (split (regexp "\n") s))
-        ) (split (regexp "\n\n") contents) 
-    )))
+    In_channel.with_open_bin file In_channel.input_all
+    |> split (regexp "\n\n")
+    |> map (fun s -> split (regexp "\n") s |> map int_of_string |> fold_left (+) 0)
+    |> sort Int.compare |> rev
+    |> hd
 
 let p2 file =
-    let contents = In_channel.with_open_bin file In_channel.input_all in
-    fold_left (fun (a, i) b -> if i < 3 then (a+b, i+1) else (a, i+1)) (0,0)
-    (rev (sort Int.compare (map (fun s ->
-        fold_left (+) 0 (
-            map (int_of_string) (split (regexp "\n") s))
-        ) (split (regexp "\n\n") contents) 
-    )))
+    In_channel.with_open_bin file In_channel.input_all 
+    |> split (regexp "\n\n")
+    |> map (fun s -> split (regexp "\n") s |> map int_of_string |> fold_left (+) 0)
+    |> sort Int.compare |> rev
+    |> fold_left (fun (a, i) b -> if i < 3 then (a+b, i+1) else (a, i+1)) (0,0)
 
 let () =
-    let (a, _) = p1 filename in
-    print_endline (string_of_int a);
+    p1 filename |> string_of_int |> print_endline;
     let (b, _) = p2 filename in
     print_endline (string_of_int b);
